9fa824c483eb64dc8fe7a0b3d3bd27bb895dae13
Angelica Lo Duca <angelica.loduca@iit.cnr.it>
2022-03-21 12:52:08 +0100

    Added Comet Experiment

---
+++ DataAnalysis/KNN.R
@@ -0,0 +1,35 @@
+library(cometr)
+library(caret)
+
+df <- read.csv('../source/breast-cancer.csv')
+df$diagnosis <- as.factor(df$diagnosis)
+
+set.seed(30)
+n <- dim(df)[1]
+for (method in c('knn', 'rpart')){
+  exp <- create_experiment()
+  
+  for (i in seq(200, n+1, by=50)) {
+    if(i > n)
+      i = n
+    dft <- df[c(1:i),]
+    
+    index <- createDataPartition(y = dft$diagnosis, times = 1, p = 0.8, list = FALSE)
+    training_set <- dft[index,]
+    test_set <- dft[-index,]
+    trControl <- trainControl(method = "repeatedcv",number = 10,repeats = 10)
+    model <- train(diagnosis ~ ., method=method, data = training_set, metric='Accuracy',
+                   preProcess = c("center","scale"), trControl=trControl)
+    
+    test_set$pred <- predict(model, test_set)
+    
+    acc <- accuracy(test_set$diagnosis, test_set$pred)
+    test_set$factor_pred <- as.factor(test_set$pred)
+    
+    exp$log_metric("accuracy", acc, step=i)
+    exp$add_tags(list(method))
+    
+  }
+  
+  exp$stop()
+}
